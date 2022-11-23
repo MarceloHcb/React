ac90b585c50417c78314ba0d68a3a13c79b1787c
MarceloHcb <marcelohcb@msn.com>
2022-11-23 12:51:28 -0300

    atualiza exercicios react

---
+++ aula-ao-vivo/secao1-dia1/src/App.js
@@ -0,0 +1,16 @@
+import Card from './Card';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>      
+      <Card/>
+    </main>
+  );
+}
+
+export default App;
---
+++ aula-ao-vivo/secao1-dia1/src/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render() {
+        // const nomes = ['Felipe', 'Maitê', 'André']
+      return ( 
+        <section>
+            <h1>TITULO</h1>
+            <img 
+            src="https://http2.mlstatic.com/D_Q_NP_2X_825358-MLA50696016765_072022-P.webp"
+            alt="imagem"
+            />
+            <p>Descrição</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque explicabo ut a totam eveniet excepturi amet aliquid repudiandae ab. Blanditiis velit quisquam, quo dolores voluptatem eum maxime numquam consequatur.</p>
+        </section>
+
+      );
+    }
+}
+export default HelloWorld;
